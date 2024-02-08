b7ed3637d2a84f44102e14d78cbf2dd99850b645
JChoy <jchoyeclipse@gmail.com>
2024-02-08 15:54:33 +0900

    feat: make mock token as permitERC20

--- src/MockToken.sol
+++ src/MockToken.sol
@@ -1,10 +1,11 @@
 // SPDX-License-Identifier: UNLICENSED
+
 pragma solidity ^0.8.13;
 
-import "@openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin-contracts/contracts/token/ERC20/extensions/ERC20Permit.sol";
 
-contract MockToken is ERC20 {
-    constructor(string memory name, string memory symbol) ERC20(name, symbol) {}
+contract MockToken is ERC20Permit {
+    constructor(string memory name, string memory symbol) ERC20(name, symbol) ERC20Permit(name) {}
 
     function mint(address to, uint256 amount) external {
         _mint(to, amount);
